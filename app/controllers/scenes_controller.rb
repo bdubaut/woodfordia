9d21be3050af27e2086d54a1a73f3bed79958989
Bertrand Dubaut <b.dubaut@gmail.com>
2014-11-14 20:10:25 +1000

    create scene OK for now

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -2,11 +2,22 @@ class ScenesController < ApplicationController
   before_action :authenticate_user!
 
   def new
-
+    @adventure = Adventure.where(id: params[:adventure_id]).first
   end
 
   def create
-
+    @adventure = Adventure.where(id: params[:adventure_id]).first
+    unless @adventure.nil?
+      s = Scene.new(scene_params)
+      if s.save
+        @adventure.scenes << s
+        @adventure.save
+        redirect_to adventure_path(params[:adventure_id]) and return
+      else
+        redirect_to new_adventure_scene_path, notice: "the scene could not be created" and return
+      end
+    end
+    redirect_to(root_path, notice: "This adventure does not exist.") and return
   end
 
   def edit
@@ -21,4 +32,9 @@ class ScenesController < ApplicationController
 
   end
 
+  private
+
+  def scene_params
+    params.require(:scene).permit(:title, :description)
+  end
 end
--- spec/controllers/scenes_controller_spec.rb
+++ spec/controllers/scenes_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ScenesController, :type => :controller do
-
+  before(:all) do
+    Adventure.delete_all
+    Scene.delete_all
+    User.delete_all
+    Role.delete_all
+    [:admin, :character, :player].each do |role|
+      Role.find_or_create_by(name: role)
+    end
+    @user = FactoryGirl.create(:admin)
+    @adventure = FactoryGirl.create(:adventure)
+  end
+  before(:each) do
+    allow(controller).to receive(:authenticate_user!).and_return(true)
+  end
+  describe '#create' do
+    before(:each) do
+      Scene.delete_all
+    end
+    it 'creates a new scene in the adventure' do
+      expect(@adventure.scenes).to be_empty
+      post :create, adventure_id: @adventure.id, scene: {title: 'Scene 01', description: 'blabla'}
+      expect(@adventure.scenes.count).to eq 1
+      expect(response.status).to eq 302
+      expect(response).to redirect_to adventure_path(@adventure.id)
+    end
+    it 'redirects to the dashboard if the adventure is not found' do
+      post :create, adventure_id: "toto", scene: {title: 'Scene 01', description: 'blabla'}
+      expect(response).to redirect_to root_path
+    end
+    it 'redirects to the new action if the creation fails.' do
+      expect(@adventure.scenes).to be_empty
+      post :create, adventure_id: @adventure.id, scene: {description: 'blabla'}
+      expect(response).to redirect_to new_adventure_scene_path(adventure_id: @adventure_id)
+    end
+  end
 end
