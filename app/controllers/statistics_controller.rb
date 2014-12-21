eb42c22e736052542b3e8faf07df4a58b9d063bb
Bertrand Dubaut <b.dubaut@gmail.com>
2014-12-21 19:42:05 +1000

    first layer of stats done.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,5 +1,6 @@
 class StatisticsController < ApplicationController
   def index
     @adventures = Adventure.all.entries
+    @users = User.with_role(:player).all.entries
   end
 end
--- app/helpers/check_ins_helper.rb
+++ app/helpers/check_ins_helper.rb
@@ -1,2 +1,10 @@
 module CheckInsHelper
+  def outcome id
+    scene = Scene.where(id: id).first
+    if scene.nil?
+      return " "
+    else
+      scene.title
+    end
+  end
 end
